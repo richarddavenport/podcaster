47f83364206e9122ecf3e233b045564a2d90ade2
Richard Davenport <rad22684@gmail.com>
2019-07-30 07:58:08 -0500

    Project creation

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<form id="test-form">
+  Input1:
+  <input name="input1" />
+  <br />
+  Input2:
+  <input name="input2" />
+  <br />
+  <input type="submit" />
+</form>
